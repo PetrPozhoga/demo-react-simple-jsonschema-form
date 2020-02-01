1cfcc90d2e10bb7b1149013154526685c25a6ef2
PetrPozhoga <pet24489@gmail.com>
2020-02-01 16:29:25 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import JsonParadiseSchema from 'json-schema100'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <JsonParadiseSchema/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.

04261d26a6ba4841f839a13da56a57d536932a88
PetrPozhoga <pet24489@gmail.com>
2020-02-04 23:09:33 +0200

    create page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import JsonParadiseSchema from 'json-schema100'
-import './App.css';
+import React from "react"
+import jsonSchema from "./schema.json" // Your custom json schema
+import './App.scss'
+import './styles/fonts.scss'
+import "react-simple-json-schema-form/build/styles.css"
+import JsonParadiseSchema from "react-simple-json-schema-form"
 
 function App() {
+
+  const send = (params, properties) => {
+    // params - received values ​​entered by the user after onSubmit
+    console.log(params)
+    console.log(properties)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <JsonParadiseSchema/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <JsonParadiseSchema schema={ jsonSchema } onSubmit={ send }/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
